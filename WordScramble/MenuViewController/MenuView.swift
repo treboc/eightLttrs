8d977c1c9a697e06b5d5d6521b7d08a4ff7e8bd7
Marvin Lee <48476808+vogelfrey@users.noreply.github.com>
2022-08-18 00:13:01 +0200

    feature: add menu
    - add a menu, currently containing buttons to restart and end the game

---
+++ WordScramble/MenuViewController/MenuView.swift
@@ -0,0 +1,74 @@
+//
+//  MenuView.swift
+//  WordScramble
+//
+//  Created by Marvin Lee Kobert on 17.08.22.
+//
+
+import UIKit
+
+class MenuView: UIView {
+  /*
+   1. Reset Session Button
+   2. End Session Button
+   3. Show Highscores Button
+   */
+  lazy var stackView: UIStackView = {
+    let sv = UIStackView()
+    sv.axis = .vertical
+    sv.alignment = .center
+    sv.spacing = 20
+    sv.translatesAutoresizingMaskIntoConstraints = false
+    return sv
+  }()
+
+  lazy var resetSessionButton: UIButton = {
+    let btn = UIButton()
+    btn.configuration = .borderedProminent()
+    btn.setTitle("Reset Session", for: .normal)
+    btn.translatesAutoresizingMaskIntoConstraints = false
+    return btn
+  }()
+
+  lazy var endSessionButton: UIButton = {
+    let btn = UIButton()
+    btn.configuration = .borderedProminent()
+    btn.setTitle("End Session", for: .normal)
+    btn.translatesAutoresizingMaskIntoConstraints = false
+    return btn
+  }()
+
+  lazy var showHighscoreButton: UIButton = {
+    let btn = UIButton()
+    btn.configuration = .borderedProminent()
+    btn.setTitle("Show Highscore", for: .normal)
+    btn.translatesAutoresizingMaskIntoConstraints = false
+    return btn
+  }()
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.backgroundColor = .systemBackground
+    setupLayout()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func setupLayout() {
+    let views = [resetSessionButton, endSessionButton, showHighscoreButton]
+    views.forEach {
+      stackView.addArrangedSubview($0)
+      $0.widthAnchor.constraint(equalTo: stackView.widthAnchor, multiplier: 0.5).isActive = true
+    }
+
+    addSubview(stackView)
+
+    NSLayoutConstraint.activate([
+      stackView.centerXAnchor.constraint(equalTo: centerXAnchor),
+      stackView.centerYAnchor.constraint(equalTo: centerYAnchor),
+    ])
+  }
+
+}
---
+++ WordScramble/MenuViewController/MenuViewController.swift
@@ -0,0 +1,58 @@
+//
+//  MenuViewController.swift
+//  WordScramble
+//
+//  Created by Marvin Lee Kobert on 17.08.22.
+//
+
+import UIKit
+
+protocol MenuViewControllerDelegate {
+  func resetGame()
+  func endGame()
+}
+
+class MenuViewController: UIViewController {
+  var menuView: MenuView {
+    view as! MenuView
+  }
+
+  var delegate: MenuViewControllerDelegate?
+
+  override func loadView() {
+    view = MenuView()
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.title = "Menu"
+    setupActions()
+    navigationController?.navigationBar.prefersLargeTitles = true
+    navigationItem.largeTitleDisplayMode = .always
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(closeMenu))
+  }
+}
+
+extension MenuViewController {
+  private func setupActions() {
+    menuView.resetSessionButton.addTarget(self, action: #selector(resetButtonTapped), for: .touchUpInside)
+    menuView.endSessionButton.addTarget(self, action: #selector(endGameButtonTapped), for: .touchUpInside)
+  }
+
+  @objc
+  private func resetButtonTapped() {
+    dismiss(animated: true)
+    delegate?.resetGame()
+  }
+
+  @objc
+  private func endGameButtonTapped() {
+    dismiss(animated: true)
+    delegate?.endGame()
+  }
+
+  @objc
+  private func closeMenu() {
+    dismiss(animated: true)
+  }
+}
