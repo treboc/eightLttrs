42c12e4791c7f5363cf865ff6c0d0a7b588ad453
Marvin Lee <48476808+vogelfrey@users.noreply.github.com>
2022-09-06 08:14:39 +0200

    refactor: abstracted presentAlertController()

--- WordScramble/Helper/Extensions/UIAlertController+Extensions.swift
+++ WordScramble/Helper/Extensions/UIAlertController+Extensions.swift
@@ -5,4 +5,24 @@
 //  Created by Marvin Lee Kobert on 06.09.22.
 //
 
-import Foundation
+import UIKit
+
+extension UIAlertController {
+  static func presentAlertController(on viewController: UIViewController,
+                                     title: String,
+                                     message: String,
+                                     onContinuePressed: ((UIAlertAction) -> Void)? = nil ) {
+    let ac = UIAlertController(title: title,
+                               message: message,
+                               preferredStyle: .alert)
+    if let handler = onContinuePressed {
+      let cancelAction = UIAlertAction(title: L10n.ButtonTitle.cancel, style: .cancel)
+      ac.addAction(cancelAction)
+      let proceedAction = UIAlertAction(title: L10n.ButtonTitle.continue, style: .destructive, handler: handler)
+      ac.addAction(proceedAction)
+    } else {
+      ac.addAction(UIAlertAction(title: L10n.ButtonTitle.ok, style: .default))
+    }
+    viewController.present(ac, animated: true)
+  }
+}
--- WordScramble/SceneDelegate.swift
+++ WordScramble/SceneDelegate.swift
@@ -99,21 +99,5 @@ extension SceneDelegate {
     return mainVC as? MainViewController
   }
 
-  private func presentAlertController(on viewController: UIViewController,
-                                      with title: String,
-                                      and message: String,
-                                      onContinuePressed: ((UIAlertAction) -> Void)? = nil ) {
-    let ac = UIAlertController(title: title,
-                               message: message,
-                               preferredStyle: .alert)
-    if let handler = onContinuePressed {
-      let cancelAction = UIAlertAction(title: L10n.ButtonTitle.cancel, style: .cancel)
-      ac.addAction(cancelAction)
-      let proceedAction = UIAlertAction(title: L10n.ButtonTitle.continue, style: .destructive, handler: handler)
-      ac.addAction(proceedAction)
-    } else {
-      ac.addAction(UIAlertAction(title: L10n.ButtonTitle.ok, style: .default))
-    }
-    viewController.present(ac, animated: true)
   }
 }
