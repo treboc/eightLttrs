c67af99e827f355d8868bd64c008f95b987e4170
Marvin Lee <48476808+vogelfrey@users.noreply.github.com>
2022-09-12 10:19:56 +0200

    refactor: GameAPI for testing
    added the NSManagedObjectContext as parameter into the methods, so it can be injected while testing

--- eightLttrs/Services/GameService/GameAPI.swift
+++ eightLttrs/Services/GameService/GameAPI.swift
@@ -6,20 +6,22 @@
 //
 
 import Combine
+import CoreData
 import Foundation
 import UIKit
 
 class GameAPI {
   var usersLocale: WSLocale
+  static let moc = PersistenceStore.shared.context
 
   init(wsLocale: WSLocale = .getStoredWSLocale()) {
     self.usersLocale = wsLocale
   }
 
-  func startGame(_ gameType: GameType) -> Session {
+  func startGame(_ gameType: GameType = .random, in context: NSManagedObjectContext = moc) -> Session {
     switch gameType {
     case .continueLastSession:
-      return SessionService.returnLastSession() ?? randomWordSession()
+      return SessionService.returnLastSession(in: context) ?? randomWordSession()
     case .shared(let word):
       return newSession(with: word)
     default:
@@ -27,8 +29,8 @@ class GameAPI {
     }
   }
 
-  func newSession(with word: String) -> Session {
-    let session: Session = .newSession(with: word)
+  func newSession(with word: String, in context: NSManagedObjectContext = moc) -> Session {
+    let session: Session = .newSession(with: word, in: context)
     let (possibleWords, maxPossibleScore) = WordService.getAllPossibleWords(for: word)
     session.possibleWordsSet = possibleWords
     session.maxPossibleWordsOnBaseWord = possibleWords.count
@@ -36,10 +38,10 @@ class GameAPI {
     return session
   }
 
-  func randomWordSession() -> Session {
+  func randomWordSession(in context: NSManagedObjectContext = moc) -> Session {
     usersLocale = .getStoredWSLocale()
     let (baseword, possibleWords, maxPossibleScore) = WordService.getNewBasewordWith(usersLocale)
-    let session: Session = .newSession(with: baseword)
+    let session: Session = .newSession(with: baseword, in: context)
     session.possibleWords = Array(possibleWords)
     session.maxPossibleWordsOnBaseWord = possibleWords.count
     session.maxPossibleScoreOnBaseWord = maxPossibleScore
@@ -47,8 +49,10 @@ class GameAPI {
     return session
   }
 
-  func continueLastSession() -> Session {
-    guard let lastSession = SessionService.returnLastSession() else { return randomWordSession() }
+  func continueLastSession(in context: NSManagedObjectContext = moc) -> Session {
+    guard let lastSession = SessionService.returnLastSession(in: context) else {
+      return randomWordSession(in: context)
+    }
     return lastSession
   }
 
